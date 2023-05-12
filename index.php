ea767b15922d12110322ab21e1615ce2d56a4277
Wesley Ryan Mulombwa <52097220+WesleyRyanMulombwa@users.noreply.github.com>
2023-05-12 16:48:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    
+	<title>Nchito - Work</title>
+
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+
+
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Search for Job</a></li>
+				<li><a href="#">Looking for Employees</a></li>
+				<li><a href="#">Login</a></li>
+				<li><a href="#">Sign Up</a></li>
+			</ul>
+		</nav>
+
+
+   <!--Content before Waves Container-->
+        <div class="inner-header flex">
+
+<h1>Nchito</h1>
+    
+   
+    </div>
+
+    <!--Waves Container-->
+<div>
+<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+<defs>
+<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+</defs>
+<g class="parallax">
+<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+</g>
+</svg>
+</div>
+<!--Waves end-->
+
+
+	</header>
+    
+	<main>
+    <img src="image.jpg" alt="Image">
+    <p>Zambias best Job hunting platform - part-time, full-time, short-term, or long-term contracts.</p>
+
+
+    <div class="promos">  
+<div class="promo">
+  <div class="deal">
+    <span>Premium</span>
+    <span>This is really a good deal!</span>
+  </div>
+  <span class="price">$79</span>
+  <ul class="features">
+    <li>Some great feature</li>
+    <li>Another super feature</li>
+    <li>And more...</li>   
+  </ul>
+  <button>Sign up</button>
+</div>
+<div class="promo scale">
+  <div class="deal">
+    <span>Plus</span>
+    <span>This is really a good deal!</span>
+  </div>
+  <span class="price">$89</span>
+  <ul class="features">
+    <li>Some great feature</li>
+    <li>Another super feature</li>
+    <li>And more...</li>   
+  </ul>
+  <button>Sign up</button>
+</div>
+<div class="promo">
+  <div class="deal">
+    <span>Basic</span>
+    <span>Basic membership</span>
+  </div>
+  <span class="price">$69</span>
+  <ul class="features">
+    <li>Choose the one on the left</li>
+    <li>We need moneyy</li>
+    <li>And more...</li>   
+  </ul>
+  <button>Sign up</button>
+</div>
+</div>
+		
+	</main>
